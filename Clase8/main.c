8d3cd2b728c5d176332fc6134d5dd0498cfa981d
EliasLeguizamon123 <elias-1997@live.com.ar>
2017-04-11 21:33:19 -0300

    Primera parte bien resuelta

--- Clase8/main.c
+++ Clase8/main.c
@@ -13,27 +13,32 @@ void main()
     int i, j;
     struct alumno pers[MAX],aux;
 
-    for(i=0; i<MAX; i++)
-        {
+    for(i=0; i<MAX; i++)                 //se piden los datos necesarios
+    {
         printf("ingrese su nombre: ");
         fflush(stdin);
         gets(pers[i].nombre);
 
-        }
+        printf("Ingrese su nota: ");
+        scanf("%d", &pers[i].nota);
 
-    for(i=0; i<MAX-1; i++)
+    }
+
+    for(i=0; i<MAX-1; i++)                  //se ordenan de la A a la Z los "nombres"
     {
         for(j=i+1; j<MAX; j++)
-            if((strcmp(pers[i].nombre,pers[j].nombre))<0)
+            if((strcmp(pers[i].nombre,pers[j].nombre))>0)
             {
                 aux=pers[i];
                 pers[i]=pers[j];
                 pers[j]=aux;
             }
 
-        printf("\n%s", pers[i]);
-
 
     }
+     for(i=0; i<MAX; i++)                              //se imprimen los nombres y sus notas respectivas
+        {
+            printf("\n%s \t%d", pers[i].nombre, pers[i].nota);
+        }
 }
 
